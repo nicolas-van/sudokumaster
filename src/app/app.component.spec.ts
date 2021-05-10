3bbab2fb52c50638ce881106fb66818199783d47
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-05-10 10:25:21 +0200

    wip

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'sudokumaster'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sudokumaster');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sudokumaster app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   });
 });
